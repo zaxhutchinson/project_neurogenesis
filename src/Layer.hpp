d524d6902ac4ca312298f58fd553d9cd8ee3c00f
zax <zaxhutchinson@gmail.com>
2018-12-06 14:48:48 -0500

    Adds Layers

---
+++ src/Layer.hpp
@@ -0,0 +1,11 @@
+#ifndef LAYER_HPP
+#define LAYER_HPP
+
+#include"spspdef.hpp"
+#include"Neuron.hpp"
+
+struct Layer {
+    vec<Neuron> neurons;
+};
+
+#endif
--- src/Sim.hpp
+++ src/Sim.hpp
@@ -1,12 +1,18 @@
 #ifndef SIM_HPP
 #define SIM_HPP
 
+#include"spspdef.hpp"
+#include"Layer.hpp"
+
 class Sim {
 public:
-
+    Sim();
+    void Build();
+    void Run();
 
 
 private:
+    vsptr<Layer> layers;
 };
 
 #endif
